f472f22aa64467807458e70a1c08c8a4b23f2103
Joshua Castellucci <joshua.castellucci@ravanallc.com>
2025-10-21 21:56:58 -0400

    Changed logiic regarding image creation and transmitting. Need to test locally before testing the push to the controller.

--- internal/deploy/main.go
+++ internal/deploy/main.go
@@ -3,6 +3,8 @@ package deploy
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
 	"os/exec"
 
 	prompts "github.com/digizyne/lf/internal/prompts"
@@ -19,11 +21,38 @@ func BuildContainerImage(tag string) error {
 	return nil
 }
 
-func PushContainerImage(tag string) error {
-	cmd := exec.Command("docker", "push", tag)
+func saveContainerImage(tag string, filename string) error {
+	cmd := exec.Command("docker", "save", tag, "|", "gzip", ">", filename)
 	err := cmd.Run()
 	if err != nil {
-		return fmt.Errorf("docker push failed: %v", err)
+		return fmt.Errorf("docker save failed: %v", err)
+	}
+
+	return nil
+}
+
+func transmitCompressedImage(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %v", filename, err)
+	}
+	defer file.Close()
+
+	req, err := http.NewRequest("POST", "https://example.com/upload", file)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/gzip")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to transmit image: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to transmit image: %v", resp.Status)
 	}
 
 	return nil
@@ -34,17 +63,21 @@ func Deploy(ctx context.Context, cmd *cli.Command) error {
 	if err != nil {
 		return fmt.Errorf("failed to get deployment name: %w", err)
 	}
+	filename := fmt.Sprintf("%s.tgz", deploymentName)
 
-	tag := "us-central1-docker.pkg.dev/jcastle-dev/local-first-public/" + deploymentName + ":latest"
-
-	err = BuildContainerImage(tag)
+	err = BuildContainerImage(deploymentName)
 	if err != nil {
 		return fmt.Errorf("failed to build container image: %v", err)
 	}
 
-	err = PushContainerImage(tag)
+	err = saveContainerImage(deploymentName, filename)
 	if err != nil {
-		return fmt.Errorf("failed to push container image: %v", err)
+		return fmt.Errorf("failed to save container image: %v", err)
+	}
+
+	err = transmitCompressedImage(filename)
+	if err != nil {
+		return fmt.Errorf("failed to transmit compressed image: %v", err)
 	}
 
 	return nil
